54c6e68776a1ee4760b04bbaca144aaa7c254b22
Faridrizqullohsaputra <149545113+Faridrizqullohsaputra@users.noreply.github.com>
2023-10-31 17:37:43 -0700

    Add files via upload

---
+++ praktikum1ddpfaridrizqulloh_tictacto.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"\t\t  TIC TOA GAME "<<endl;
+	cout<<"   player[x] \N player[o]"<<endl;
+	cout<<"\t\t\t     |    |     "<<endl;
+	cout<<"\t\t\t  s  | o  |s    "<<endl;
+	cout<<"\t\t\t_____|____|_____"<<endl;
+	cout<<"\t\t\t     |    |     "<<endl;
+	cout<<"\t\t\t  1  | 2  |  3  "<<endl;
+	cout<<"\t\t\t_____|____|_____"<<endl;
+	cout<<"\t\t\t     |    |     "<<endl;
+	cout<<"\t\t\t  4  | 5  |  6  "<<endl;
+	cout<<"\t\t\t_____|____|_____"<<endl;
+	cout<<"\t\t\t     |    |     "<<endl;
+	cout<<"\t\t\t  7  | 8  |  9  "<<endl;
+	cout<<"\t\t\t_____|____|_____"<<endl;
+	cout<<endl;
+	cout<<"\t\t\  player1[x] Turn: " ;
+	
+	
+} 
+		
+	
+	
+	
+
